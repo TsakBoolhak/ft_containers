ec0fac50fa974003c1daa14504e999b9b75137fd
TsakBoolhak <tsakboolhak@gmail.com>
2022-03-29 19:54:03 +0200

    reverse_iterator and iterator_traits done ?

---
+++ iterator_traits.hpp
@@ -0,0 +1,38 @@
+#ifndef ITERATOR_TRAITS_HPP
+# define ITERATOR_TRAITS_HPP
+namespace ft {
+
+	template< class Iterator >
+	struct iterator_traits {
+
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+		typedef typename Iterator::iterator_category	iterator_category;
+
+	};
+
+	template< class T > struct iterator_traits< T* > {
+
+		typedef typename ptrdiff_t					difference_type;
+		typedef typename T							value_type;
+		typedef typename T*							pointer;
+		typedef typename T&							reference;
+		typedef typename random_access_iterator_tag	iterator_category;
+
+	}; 
+
+	template< class T > struct iterator_traits< const T* > {
+
+		typedef typename ptrdiff_t					difference_type;
+		typedef typename T							value_type;
+		typedef typename const T*					pointer;
+		typedef typename const T&					reference;
+		typedef typename random_access_iterator_tag	iterator_category;
+
+	};
+
+}; 
+
+#endif
