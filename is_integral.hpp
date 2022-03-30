1a8b9041e1e6ec77da32eeafd36d31aa63e4ac45
TsakBoolhak <tsakboolhak@gmail.com>
2022-03-30 14:33:02 +0200

    added enable_if, equal, lexicographical_compare, is_integer and pair

---
+++ is_integral.hpp
@@ -0,0 +1,131 @@
+#ifndef IS_INTEGRAL_HPP
+# define IS_INTEGRAL_HPP
+
+namespace ft {
+
+	template < typename >
+	struct is_integral_base {
+
+		static bool const value = false;
+	};
+
+	template <>
+	struct is_integral_base< bool > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< char > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< unsigned char > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< signed char > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< char16_t > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< char32_t > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< wchar_t > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< short int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< unsigned short int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< signed short int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< unsigned int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< signed int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< long int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< unsigned long int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< signed long int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< long long int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< unsigned long long int > {
+
+		static bool const value = true;
+	};
+
+	template <>
+	struct is_integral_base< signed long long int > {
+
+		static bool const value = true;
+	};
+
+	template< typename T >
+	struct is_integral : is_integral_base< T > {};
+
+}
+
+#endif
---
+++ pair.hpp
@@ -0,0 +1,84 @@
+#ifndef PAIR_HPP
+# define PAIR_HPP
+
+namespace ft {
+
+	template< class T1, class T2 >
+	struct pair {
+
+		T1	first;
+		T2	second;
+
+		pair() : first ( T1() ), second( T2() ) {
+
+			return ;
+		}
+
+		pair( const T1 & x, const T2 & y) : first ( x ), second ( y ) {
+
+			return ;
+		}
+
+		template< class U, class V >
+		pair( const pair< U, V > &p ) : first ( p.first ), second ( p.second) {
+
+			return;
+		}
+
+		pair &	operator=( const pair & other ) {
+
+			if (this != &other) {
+
+				this->first = other.first;
+				this->second = other.second;
+			}
+			return *this;
+		}
+
+	};
+
+	template< class T1, class T2 >
+	bool	operator==( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return x.first == y.first && x.second == y.second;
+	}
+
+	template< class T1, class T2 >
+	bool	operator!=( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return !(x == y);
+	}
+
+	template< class T1, class T2 >
+	bool	operator<( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return x.first < y.first || ( !( y.first < x.first ) && x.second < y.second );
+	}
+
+	template< class T1, class T2 >
+	bool	operator<=( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return !(y < x);
+	}
+
+	template< class T1, class T2 >
+	bool	operator>( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return y < x;
+	}
+
+	template< class T1, class T2 >
+	bool	operator>=( const pair< T1, T2 > & x, const pair< T1, T2 > & y ) {
+
+		return !(x < y);
+	}
+
+	template< class T1, class T2 >
+	pair< T1, T2 >	make_pair( const T1 & x, const T2 & y ) {
+
+		return pair< T1, T2 >( x, y );
+	}
+
+}
+
+#endif
